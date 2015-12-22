0ab4861696ce60de8d4fe4af36e0d365c7bfccff
mufc <mufc@test.com>
2015-12-22 17:23:30 +0800

    第一次提交初始化工程

---
+++ gitDemo/gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by QianFeng on 15/12/22.
+//  Copyright © 2015年 张潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by QianFeng on 15/12/22.
+//  Copyright © 2015年 张潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
